cda12e4d43c655805722f66076a774f43340ab27
matheusbyjus <129328572+matheusbyjus@users.noreply.github.com>
2023-09-21 15:57:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+})
+
+camera=document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function foto(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML=`<img id="captura" src="${data_uri}"/>`;
+    })
+}
+
+console.log("ml5:",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/VDpbVidrY/.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("modelo carregado")
+}
+
+function checar(){
+    img=document.getElementById("captura");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error)
+    } else{
+        console.log(results);
+        document.getElementById("nomeObjeto").innerHTML=results[0].label;
+        document.getElementById("precisao").innerHTML=results[0].confidence.toFixed(2);
+    }
+}
